4130b5a0b5ee3a22c4576eeb53b1cb12d6d98604
Chupong CK Kitisopakul <chupong.kitisopakul@gmail.com>
2019-01-20 15:38:54 +1100

    feat(challenge5 stack): my attempt

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -1,11 +1,45 @@
+import { listenerCount } from "cluster";
+
 interface IStack<T> {
   push(item: T): IStack<T>;
   push(items: T[]): IStack<T>;
   pop(): T | undefined;
   length(): number;
-  print(): void;
+  // print(): void;
 }
 
-// export class Stack<T> {
+export class Stack<T> implements IStack<T> {
+  private list: Array<T>;
 
-// }
+  constructor() {
+    this.list = new Array<T>();
+  }
+
+  length(): number {
+    return this.list.length;
+  }
+
+  push(item: T): IStack<T>;
+  push(items: T[]): IStack<T>;
+
+  push(items: T | T[]): IStack<T> {
+    if (items instanceof Array) {
+      this.list.push(...items);
+    } else {
+      this.list.push(items);
+    }
+    return this;
+  }
+
+  pop(): T | undefined {
+    if (this.list.length === 0) {
+      return undefined;
+    } else {
+      return this.list.pop();
+    }
+  }
+}
+
+
+// let s = new Stack<number>();
+// console.log(s.length());
