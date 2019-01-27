4bc12371c7a559a4edc0d12efb6619cc9b050ebd
Chupong CK Kitisopakul <chupong.kitisopakul@gmail.com>
2019-01-27 23:04:01 +1100

    feat(challenge6): fibonacci

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -3,6 +3,16 @@
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+
+export function* getFibSequence(): IterableIterator<number> {
+  let last = 1;
+  let previousLast = 0;
+  let currentResult;
+  yield last;
+  while(true) {
+    currentResult = last + previousLast;
+    yield currentResult;
+    previousLast = last;
+    last = currentResult;
+  }
+}
